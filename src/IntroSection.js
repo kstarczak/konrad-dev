fe50a6d15afd30ca6434ae90b9e9075153e723ed
Konrad Starczak <k.starczak@gmail.com>
2022-08-19 17:47:58 -0400

    Convert main component to class and ref to subcomponenents

--- src/IntroSection.js
+++ src/IntroSection.js
@@ -1,14 +1,41 @@
+import { useState } from "react";
+
 const IntroSection = () => {
+    const [expandIntro, setExpandIntro] = useState(false)
+
+    const handleClick = () => {
+        if (expandIntro) {
+            setExpandIntro(false);
+        } else {
+            setExpandIntro(true);
+        }
+        
+    }
+
     return ( 
-        <section className="intro" aria-labelledby="headline">
+        <section className="intro" aria-labelledby="journey">
                     <div>
                         <small>Welcome to my home page!</small>
-                        <h2 id="headline">My Journey</h2>
+                        <h2 id="journey">My Journey</h2>
                     </div>
-                    <p>My path to web developement started about a year ago when I realized it wast time for me to make a big change.
-                            I was working as a physician assistant in emergency medicine for many years and I had come to the realization that despite my success, I found something to be lacking. I had always loved creating things as far as I can remember...legos and origami and. While studying for my undergraduate degree and workign as an EMT, I utimately decide to purse a masters degree and began a...
-                            Though this change is dramatic to those, I feel at home creating and using my imagination and problem solving skills.
-                    </p>
+            <p>
+                {!expandIntro && <span>As far as I can remember, I've had a passion for creating and building.
+                    It started with legos then it progressed to orgigami, sketching, and painting...</span>}
+                {expandIntro && <span>As far as I can remember, I've had a passion for creating and building.
+                    It all started with legos then progressed to orgigami, sketching, and painting.
+                    Towards the end of high school, my interest in biology became my focus and my creative interests were set aside.
+                    I pursued a path to help others using what I learned, first as an EMT, then later as a Physician Assistant.
+                    I contined my career in medicine for several years, specializing in both emergency medicine and orthopaedics.
+                    Then during the beginning of pandemic, I finally had time to slow down and reflect.
+                    After a few months of deliberation, I finally decided to switch to a career that would allow my to utilize my creativity and better align with my personal goals.
+                    Using the grit I cultivated working in emergency medicine for several years, I taught myself how to become a web developer.
+                    Now, when I work on projects I feel right at home using my creativity and problem solving skills :)</span>}
+                
+                
+            </p>
+            {!expandIntro && <button onClick={handleClick}>Read More</button>}
+            {expandIntro && <button onClick={handleClick}>Read Less</button>}
+            
             </section>
      );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,35 @@
+import React from 'react';
+
 import IntroSection from "./IntroSection";
 import SliderSection from "./SliderSection";
 import ContactSection from "./ContactSection";
 
 
-const Main = () => {
 
-/*
- //// add href below   and change opacity of section back to 0!
 
-    const fadeUp = (items) => {
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.introRef = React.createRef();
+        this.sliderRef = React.createRef();
+        this.contactRef = React.createRef();
+    }
+    render() {
+        return (
+            <main>
+                <IntroSection ref={this.introRef} />
+                <SliderSection ref={this.sliderRef} />
+                <ContactSection ref={this.contactRef} />
+            </main>
+            
+        )
+    }
+}
+
+
+ //// add ref below   and change opacity of section back to 0!
+
+ /*   const fadeUp = (items) => {
         items.forEach((item) => {
           if (item.isIntersecting) {
             item.target.classList.add('fade-up');
@@ -19,17 +40,10 @@ const Main = () => {
     
     const fadeUpIO =  new IntersectionObserver(fadeUp, {threshold: .6});
     
-    fadeUpIO.observe(IntroSection);
-    fadeUpIO.observe(SliderSection);
-    fadeUpIO.observe(ContactSection);
+    fadeUpIO.observe(this.introRef.current);
+    fadeUpIO.observe(this.sliderRef.current);
+    fadeUpIO.observe(this.contactRef.current);
+
 */
-    return ( 
-        <main>
-            <IntroSection/>
-            <SliderSection/>
-            <ContactSection/>
-        </main>
-     );
-}
- 
+
 export default Main;
